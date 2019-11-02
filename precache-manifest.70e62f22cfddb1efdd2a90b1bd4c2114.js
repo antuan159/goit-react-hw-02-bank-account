69bd85f11421c77413d7d8d4edec863e4da8bafd
antuan159 <lanos159@ukr.net>
2019-11-02 17:23:33 +0200

    Updates

---
+++ precache-manifest.70e62f22cfddb1efdd2a90b1bd4c2114.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a31d90c515c1ee4a88ce91645d82fd5",
+    "url": "/goit-react-hw-02-bank-account/index.html"
+  },
+  {
+    "revision": "a447a3c326dc128a0ddf",
+    "url": "/goit-react-hw-02-bank-account/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "f59eb4f8c1e54cb14448",
+    "url": "/goit-react-hw-02-bank-account/static/css/main.6ef52a5b.chunk.css"
+  },
+  {
+    "revision": "a447a3c326dc128a0ddf",
+    "url": "/goit-react-hw-02-bank-account/static/js/2.ff670185.chunk.js"
+  },
+  {
+    "revision": "f59eb4f8c1e54cb14448",
+    "url": "/goit-react-hw-02-bank-account/static/js/main.f451a5c2.chunk.js"
+  },
+  {
+    "revision": "a14a5e0139a450479bb8",
+    "url": "/goit-react-hw-02-bank-account/static/js/runtime-main.816d81fb.js"
+  }
+]);
